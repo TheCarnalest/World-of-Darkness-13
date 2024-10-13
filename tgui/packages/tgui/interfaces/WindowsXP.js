536c7bd82da2162750bbd1de09e880f217c4a3d4
TheCarnalest <167369312+TheCarnalest@users.noreply.github.com>
2024-10-13 20:54:53 +0200

    Made WindowsXP back into a class rather than a pure function

--- tgui/packages/tgui/interfaces/WindowsXP.js
+++ tgui/packages/tgui/interfaces/WindowsXP.js
@@ -3,11 +3,13 @@ import { WinXP } from "./WindowsXP/index";
 import { useBackend } from "../backend";
 import { Component } from 'inferno';
 
-const WindowsXP = () => {
-  const { act, data } = useBackend();
-  return (
-    <Window width={1200} height={678} theme="light">
-      <WinXP data={data} act={act} />
-    </Window>
-  );
+export class WindowsXP extends Component {
+  render() {
+    const { act, data } = useBackend(this.context);
+    return (
+      <Window width={1200} height={678} theme="light">
+        <WinXP data={data} act={act} />
+      </Window>
+    );
+  };
 };
